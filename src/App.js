4384bebf472cb5886e969e130e4eae8976b755dd
HariKiran <harikiran.kante@gmail.com>
2022-05-16 10:49:46 +0530

    added git ignore and removed few react imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import React, { useState } from 'react';
-import './style.css';
-import Player from './Player.js';
-import Menu from './Menu.js';
+import React, { useState } from "react";
+import "./style.css";
+import Player from "./Player.js";
+import Menu from "./Menu.js";
 
 var status = 0;
 var gameOver = false;
@@ -20,114 +19,145 @@ function Card(props) {
 
 export default class App extends React.Component {
   cards = [
-    { symbol: '' },
-    { symbol: '' },
-    { symbol: '' },
-    { symbol: '' },
-    { symbol: '' },
-    { symbol: '' },
-    { symbol: '' },
-    { symbol: '' },
-    { symbol: '' }
+    { symbol: "" },
+    { symbol: "" },
+    { symbol: "" },
+    { symbol: "" },
+    { symbol: "" },
+    { symbol: "" },
+    { symbol: "" },
+    { symbol: "" },
+    { symbol: "" },
   ];
   state = {
     cardList: this.cards,
-    result: '',
-    player:['Player 1','Player 2'],
+    result: "",
+    player: ["Player 1", "Player 2"],
   };
-  matchCards=(c1,c2,c3,symbol)=>{
-    return (this.cards[c1].symbol == symbol &&
-    this.cards[c2].symbol == symbol &&
-    this.cards[c3].symbol == symbol);
-  }
-  checkTie=()=>{
-    let allCards = [0,1,2,3,4,5,6,7,8];
-    return allCards.every((val)=>{return this.cards[val].symbol != ''});
-  }
-  checkWinner = symbol => {
-    if (this.matchCards(0,1,2,symbol)|| this.matchCards(3,4,5,symbol) || this.matchCards(6,7,8,symbol)||this.matchCards(0,3,6,symbol)||this.matchCards(1,4,7,symbol)||this.matchCards(2,5,8,symbol)||this.matchCards(0,4,8,symbol)||this.matchCards(2,4,6,symbol)) {
-      if(status == 1 ){
+  matchCards = (c1, c2, c3, symbol) => {
+    return (
+      this.cards[c1].symbol == symbol &&
+      this.cards[c2].symbol == symbol &&
+      this.cards[c3].symbol == symbol
+    );
+  };
+  checkTie = () => {
+    let allCards = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    return allCards.every((val) => {
+      return this.cards[val].symbol != "";
+    });
+  };
+  checkWinner = (symbol) => {
+    if (
+      this.matchCards(0, 1, 2, symbol) ||
+      this.matchCards(3, 4, 5, symbol) ||
+      this.matchCards(6, 7, 8, symbol) ||
+      this.matchCards(0, 3, 6, symbol) ||
+      this.matchCards(1, 4, 7, symbol) ||
+      this.matchCards(2, 5, 8, symbol) ||
+      this.matchCards(0, 4, 8, symbol) ||
+      this.matchCards(2, 4, 6, symbol)
+    ) {
+      if (status == 1) {
         this.setState({ result: `🎉 ${this.state.player[0]}  Won 🏆` });
-        gameOver=true;
-      }else{
+        gameOver = true;
+      } else {
         this.setState({ result: `🎉 ${this.state.player[1]}  Won 🏆` });
-        gameOver=true;
+        gameOver = true;
       }
-    }else if(this.checkTie()){
-      this.setState({ result: 'Game is Tied 💪' });
-      gameOver=true;
+    } else if (this.checkTie()) {
+      this.setState({ result: "Game is Tied 💪" });
+      gameOver = true;
     }
   };
-  handleClick = id => {
-    if (status == 0 && gameOver==false) {
-      this.cards[id] = { symbol: 'close' };
+  handleClick = (id) => {
+    if (status == 0 && gameOver == false) {
+      this.cards[id] = { symbol: "close" };
       status = 1;
-    } else if (status == 1 && gameOver==false) {
-      this.cards[id] = { symbol: 'radio_button_unchecked' };
+    } else if (status == 1 && gameOver == false) {
+      this.cards[id] = { symbol: "radio_button_unchecked" };
       status = 0;
-    } else{
-      alert("Game Over! Reset the Board to Play again.")
+    } else {
+      alert("Game Over! Reset the Board to Play again.");
     }
     this.setState({ cardList: this.cards });
   };
   handleReset = () => {
-    this.cards.forEach(card => {
-      card.symbol = '';
+    this.cards.forEach((card) => {
+      card.symbol = "";
     });
-    this.setState({ cardList: this.cards,result:'' });
-    gameOver=false;
+    this.setState({ cardList: this.cards, result: "" });
+    gameOver = false;
   };
-  handlePlayerName = (el,i)=>{
+  handlePlayerName = (el, i) => {
     let players = this.state.player;
     players[i] = el.target.value;
-    this.setState({player:players})
+    this.setState({ player: players });
   };
-  handelStart =()=>{
-    document.getElementById("board").classList.remove('board');
-    document.getElementById("btnReset").classList.remove('board');
-    document.getElementById("btnBack").classList.remove('board');
-    document.getElementById("menu").classList.add('board');
+  handelStart = () => {
+    document.getElementById("board").classList.remove("board");
+    document.getElementById("btnReset").classList.remove("board");
+    document.getElementById("btnBack").classList.remove("board");
+    document.getElementById("menu").classList.add("board");
   };
-  handleBack = ()=>{
-    document.getElementById("board").classList.add('board');
-    document.getElementById("btnReset").classList.add('board');
-    document.getElementById("btnBack").classList.add('board');
-    document.getElementById("menu").classList.remove('board');  
+  handleBack = () => {
+    document.getElementById("board").classList.add("board");
+    document.getElementById("btnReset").classList.add("board");
+    document.getElementById("btnBack").classList.add("board");
+    document.getElementById("menu").classList.remove("board");
   };
   render() {
     return (
-      <div id='container'>
-        <Player stat={status} playerName={this.state.player} playerIndex={0} changePlayerName={this.handlePlayerName}/>
-          <div id="game">
-          
-            <h1>Tic-Tac-Toe</h1>
-            <hr />
-            <Menu handleClick={this.handelStart} playerName={this.state.player} changePlayerName={this.handlePlayerName}/>
-            <div className="board" id="board">
-              {this.state.cardList.map((card, index) => {
-                return (
-                  <Card
-                    key={index}
-                    changeSymbol={() => {
-                      this.handleClick(index);
-                      this.checkWinner('close');
-                      this.checkWinner('radio_button_unchecked'); 
-                    }}
-                    symbol={card.symbol}
-                    checkresult ={()=>{this.checkWinner('close');}}
-                  />
-                );
-              })}
-            </div>
-            <div id="btns">
-            <p id='btnReset' className="board" onClick={this.handleReset}>Reset Board</p>
-            <p id='btnBack' className="board" onClick={this.handleBack}>Back</p>
-            </div>
-            <p id='result'>{this.state.result}</p>
+      <div id="container">
+        <Player
+          stat={status}
+          playerName={this.state.player}
+          playerIndex={0}
+          changePlayerName={this.handlePlayerName}
+        />
+        <div id="game">
+          <h1>Tic-Tac-Toe</h1>
+          <hr />
+          <Menu
+            handleClick={this.handelStart}
+            playerName={this.state.player}
+            changePlayerName={this.handlePlayerName}
+          />
+          <div className="board" id="board">
+            {this.state.cardList.map((card, index) => {
+              return (
+                <Card
+                  key={index}
+                  changeSymbol={() => {
+                    this.handleClick(index);
+                    this.checkWinner("close");
+                    this.checkWinner("radio_button_unchecked");
+                  }}
+                  symbol={card.symbol}
+                  checkresult={() => {
+                    this.checkWinner("close");
+                  }}
+                />
+              );
+            })}
           </div>
-          <Player stat={status} playerName={this.state.player} playerIndex={1} changePlayerName={this.handlePlayerName}/>
+          <div id="btns">
+            <p id="btnReset" className="board" onClick={this.handleReset}>
+              Reset Board
+            </p>
+            <p id="btnBack" className="board" onClick={this.handleBack}>
+              Back
+            </p>
+          </div>
+          <p id="result">{this.state.result}</p>
+        </div>
+        <Player
+          stat={status}
+          playerName={this.state.player}
+          playerIndex={1}
+          changePlayerName={this.handlePlayerName}
+        />
       </div>
     );
   }
 }
-
--- src/Player.js
+++ src/Player.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import React, { useState } from 'react';
-import './style.css';
+import React, { useState } from "react";
+import "./style.css";
 
 export default class Player extends React.Component {
   state = {
     person: this.props.playerName,
-    svg: ''
+    svg: "",
   };
   avatardivId = `avatar${this.props.playerIndex}`;
   playerCardId = `playerCard${this.props.playerIndex}`;
-  handleChange = el => {
+  handleChange = (el) => {
     this.props.changePlayerName(el, this.props.playerIndex);
     this.setState({ person: el.target.value });
   };
   changeAvatar = () => {
     let avatarId = this.state.person;
-    fetch('https://api.multiavatar.com/' + JSON.stringify(avatarId))
-      .then(res => res.text())
-      .then(svg => {
+    fetch("https://api.multiavatar.com/" + JSON.stringify(avatarId))
+      .then((res) => res.text())
+      .then((svg) => {
         document.getElementById(this.avatardivId).innerHTML = svg;
       });
   };
@@ -27,7 +26,7 @@ export default class Player extends React.Component {
         <div id={this.avatardivId}>
           <img src="https://www.svgrepo.com/show/13656/user.svg" alt="Avatar" />
         </div>
-        <input 
+        <input
           type="text"
           onChange={this.handleChange}
           onBlur={this.changeAvatar}
@@ -36,11 +35,11 @@ export default class Player extends React.Component {
         />
         <div id="turn">
           <p className="material-icons">
-            {this.props.playerIndex == 0 ? 'close' : 'radio_button_unchecked'}
+            {this.props.playerIndex == 0 ? "close" : "radio_button_unchecked"}
           </p>
           <p>
-            {this.props.stat == 0 && this.props.playerIndex == 0 ? '👆' : ''}
-            {this.props.stat == 1 && this.props.playerIndex == 1 ? '👆' : ''}
+            {this.props.stat == 0 && this.props.playerIndex == 0 ? "👆" : ""}
+            {this.props.stat == 1 && this.props.playerIndex == 1 ? "👆" : ""}
           </p>
         </div>
       </div>
